1b23ea0854360d15a4894df5e51c4de92e3d8a5c
Gamal Mohamed El Sayed <hp@DESKTOP-OC1QC1U>
2024-10-08 17:22:05 +0300

    Add project files.

---
+++ BuilderBundlerMinifier/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Function to change the background color of the body
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+// Function to display a welcome message
+function displayWelcomeMessage() {
+    alert('Welcome to the dummy JavaScript test!');
+}
